6ad9b3b55ad96fbc211dbc9de9613be7f7c389c9
Jing Xia <jingxia615@gmail.com>
2017-09-10 03:31:12 +0000

    new model

--- model.py
+++ model.py
@@ -72,7 +72,7 @@ flags = tf.app.flags
 FLAGS = flags.FLAGS
 
 # DEFINE FLAGS VARIABLES#
-flags.DEFINE_integer('epo', 9, "The number of epochs.")
+flags.DEFINE_integer('epo', 5, "The number of epochs.")
 flags.DEFINE_integer('batch', 32, "batch size")
 
 # Samples of the data.
