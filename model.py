666eae6e1cdb24115f234ca3a1e14f8cb32f4a98
Jing Xia <jingxia615@gmail.com>
2017-09-14 05:15:44 +0000

    add dropout
    try
    
    try
    
    try
    
    try another model structure

--- model.py
+++ model.py
@@ -133,7 +133,7 @@ def generator(samples, batch_size=FLAGS.batch):
                 line = batch_samples.iloc[i]
                 steering_center = float(line['steering'])
                 ## Correction for left and right images so that car doesn't go off the track.
-                correction = 0.4
+                correction = 0.3
                 ## center image 
                 amount = int(random.uniform(20, 50))
                 #if (abs(steering_center) >= 0.05):
@@ -214,18 +214,34 @@ validation_generator = generator(validation_samples)
 ## Model is same as the model mentioned in the NVIDIA paper.
 ### Added drop out layers extra on FC to avoid overfitting.
 model = Sequential()
-model.add(Cropping2D(cropping=((70,24), (0,0)), input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x/255 - 0.5,input_shape=(160, 320, 3)))
+model.add(Cropping2D(cropping=((70,25),(0,0))))
+model.add(Convolution2D(24, 5, 5, subsample=(2,2),activation='relu'))
+model.add(Convolution2D(36, 5, 5,subsample=(2,2), activation='relu'))
+model.add(Convolution2D(48, 5, 5,subsample=(2,2), activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
+model.add(Dense(1))
+
+model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
+"""
+model = Sequential()
+model.add(Cropping2D(cropping=((50,24), (0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x:x/255.0 - 0.5))
 model.add(Conv2D(24, (5,5), strides=(2,2), padding='valid', activation='elu'))
-#model.add(Dropout(0.25))
+model.add(Dropout(0.25))
 model.add(Conv2D(36, (5,5), strides=(2,2), padding='valid', activation='elu'))
-#model.add(Dropout(0.5))
+model.add(Dropout(0.5))
 model.add(Conv2D(48, (5,5), strides=(2,2), padding='valid', activation='elu'))
-#model.add(Dropout(0.5))
+model.add(Dropout(0.5))
 model.add(Conv2D(64, (3,3), strides=(1,1), padding='valid', activation='elu'))
 model.add(Conv2D(64, (3,3), strides=(1,1), padding='valid', activation='elu'))
 model.add(Flatten())
-#model.add(Dense(1164, activation='elu'))
+model.add(Dense(1164, activation='elu'))
 model.add(Dropout(0.5))
 model.add(Dense(100, activation='elu'))
 model.add(Dropout(0.2))
@@ -233,12 +249,12 @@ model.add(Dense(50, activation='elu'))
 model.add(Dropout(0.2))
 model.add(Dense(10 , activation='elu'))
 model.add(Dense(1))
-#adam = Adam(lr=0.001)
+adam = Adam(lr=0.001)
 model.compile(loss='mse', optimizer = 'adam', metrics=['accuracy'])
+"""
 model.summary()
 model.fit_generator(train_generator, samples_per_epoch= \
     (len(train_samples)/FLAGS.batch), validation_data= validation_generator, 
-    validation_steps=(len(validation_samples)/FLAGS.batch), epochs=FLAGS.epo,
-    verbose=1)
+    validation_steps=(len(validation_samples)/FLAGS.batch), epochs=FLAGS.epo)
 ### Save the model 
 model.save('model.h5')
